2eef4f76728071e7b824434c9a46d7b7f189bb49
Ubuntu <ubuntu@ip-172-31-80-5.ec2.internal>
2024-08-13 19:43:46 +0000

    system resouce bash

---
+++ system_resources_test.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+
+CPU_Threshold=65
+Mem_Threshold=65
+Disk_Threshold=65
+
+CPU_Usage=$(top -bn2 | grep "Cpu(s)" | tail -1 | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100-$1}')
+#-n2:use second iteration avoid first loop since boot
+if (( $(echo "$CPU_Usage > $CPU_Threshold" | bc -l) ))
+then
+  exit 1
+else
+ echo "CPU usage normal"
+fi 
+
+
+Mem_Usage=$(free | grep "Mem" | awk '{print $3/$2 * 100.0}')
+if (( $(echo  "$Mem_Usage > $Mem_Threshold" | bc -l) ))
+then
+  exit 1
+else
+ echo "Memory usage normal"
+fi 
+
+
+Disk_Usage=$(df / | grep / | awk '{ print $5}' | sed 's/%//g')
+if (( $(echo "$Disk_Usage > $Disk_Threshold" | bc -l) ))
+then
+  exit 1
+else
+ echo "Disk usage normal"
+fi 
+
+exit 0
